030cbbe5df9051d697910326ca042c880ed6025b
Кирилл Чернов <89049125360@mail.ru>
2026-02-13 18:18:48 +0300

    Initial Commit

---
+++ NewsStreamApp/NewsStreamAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsStreamAppApp.swift
+//  NewsStreamApp
+//
+//  Created by Кирилл on 13.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct NewsStreamAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
